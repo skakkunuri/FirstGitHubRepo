730cc81b93a8f3e3d14b676b44234e6e5eb0e135
skakkunuri <srikanth.kakkunuri@gmail.com>
2016-04-07 13:11:05 +0530

    Removed unused using

--- MVC_PartialView_Demo/Controllers/StudentController.cs
+++ MVC_PartialView_Demo/Controllers/StudentController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Diagnostics;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using MVC_PartialView_Demo.Models;
 
